7ba9bb429321f22490e55487d1a96bea431dec0b
Avedik <aviedik.mltykhian@mail.ru>
2024-09-15 16:43:11 +0300

    Добавил возможность открытия изображения

--- Lab2/ThirdTask/Form1.cs
+++ Lab2/ThirdTask/Form1.cs
@@ -3,15 +3,19 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Imaging;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using FastBitmap;
 
 namespace ThirdTask
 {
     public partial class Form1 : Form
     {
+        private Bitmap bitmap;
+
         public Form1()
         {
             InitializeComponent();
@@ -19,7 +23,18 @@ namespace ThirdTask
 
         private void button1_Click(object sender, EventArgs e)
         {
+            using (OpenFileDialog dialog = new OpenFileDialog())
+            {
+                dialog.Title = "Открыть изображение";
+                dialog.Filter = "Image Files|" + string.Join(";",
+                    ImageCodecInfo.GetImageEncoders().Select(encoder => encoder.FilenameExtension));
 
+                if (dialog.ShowDialog() == DialogResult.OK)
+                {
+                    bitmap = new Bitmap(dialog.FileName);
+                    pictureBox1.Image = bitmap;
+                }
+            }
         }
         private void button2_Click(object sender, EventArgs e)
         {
