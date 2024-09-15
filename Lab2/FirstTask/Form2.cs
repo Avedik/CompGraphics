6c7accded61bd5bc7bce3ed88d86442208cde6b5
ompus <babincevva@ya.ru>
2024-09-15 17:50:46 +0400

    First task is done (with basic Bitmap)

---
+++ Lab2/FirstTask/Form2.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace FirstTask
+{
+    public partial class Form2 : Form
+    {
+        public int[] hist1;
+        public int[] hist2;
+        public int[] hist3;
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void Form2_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (e.CloseReason == CloseReason.UserClosing)
+            {
+                e.Cancel = true;
+                Hide();
+            }
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            int[] nums = new int[256];
+            for (int i = 0; i < 256; ++i)
+                nums[i] = i;
+            chart1.Series["Кол-во"].Points.DataBindXY(nums, hist1);
+            chart2.Series["Кол-во"].Points.DataBindXY(nums, hist2);
+            chart3.Series["Кол-во"].Points.DataBindXY(nums, hist3);
+        }
+    }
+}
