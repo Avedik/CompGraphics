ea042062bd1d05a273ba0a2725617e0da5439c94
Dan11l54 <94974300+Dan11l54@users.noreply.github.com>
2024-09-16 03:10:07 +0300

    Task 2 is completed

--- Lab2/SecondTask/Form1.Designer.cs
+++ Lab2/SecondTask/Form1.Designer.cs
@@ -28,13 +28,116 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnLoadImage = new System.Windows.Forms.Button();
+            this.pbOriginal = new System.Windows.Forms.PictureBox();
+            this.pbRedChannel = new System.Windows.Forms.PictureBox();
+            this.pbGreenChannel = new System.Windows.Forms.PictureBox();
+            this.pbBlueChannel = new System.Windows.Forms.PictureBox();
+            this.panelRedHistogram = new System.Windows.Forms.Panel();
+            this.panelGreenHistogram = new System.Windows.Forms.Panel();
+            this.panelBlueHistogram = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.pbOriginal)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbRedChannel)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbGreenChannel)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbBlueChannel)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnLoadImage
+            // 
+            this.btnLoadImage.Location = new System.Drawing.Point(307, 334);
+            this.btnLoadImage.Name = "btnLoadImage";
+            this.btnLoadImage.Size = new System.Drawing.Size(75, 23);
+            this.btnLoadImage.TabIndex = 0;
+            this.btnLoadImage.Text = "Загрузить";
+            this.btnLoadImage.UseVisualStyleBackColor = true;
+            // 
+            // pbOriginal
+            // 
+            this.pbOriginal.Location = new System.Drawing.Point(21, 205);
+            this.pbOriginal.Name = "pbOriginal";
+            this.pbOriginal.Size = new System.Drawing.Size(256, 256);
+            this.pbOriginal.TabIndex = 1;
+            this.pbOriginal.TabStop = false;
+            // 
+            // pbRedChannel
+            // 
+            this.pbRedChannel.Location = new System.Drawing.Point(352, 32);
+            this.pbRedChannel.Name = "pbRedChannel";
+            this.pbRedChannel.Size = new System.Drawing.Size(256, 256);
+            this.pbRedChannel.TabIndex = 2;
+            this.pbRedChannel.TabStop = false;
+            // 
+            // pbGreenChannel
+            // 
+            this.pbGreenChannel.Location = new System.Drawing.Point(614, 32);
+            this.pbGreenChannel.Name = "pbGreenChannel";
+            this.pbGreenChannel.Size = new System.Drawing.Size(256, 256);
+            this.pbGreenChannel.TabIndex = 3;
+            this.pbGreenChannel.TabStop = false;
+            // 
+            // pbBlueChannel
+            // 
+            this.pbBlueChannel.Location = new System.Drawing.Point(876, 32);
+            this.pbBlueChannel.Name = "pbBlueChannel";
+            this.pbBlueChannel.Size = new System.Drawing.Size(256, 256);
+            this.pbBlueChannel.TabIndex = 4;
+            this.pbBlueChannel.TabStop = false;
+            // 
+            // panelRedHistogram
+            // 
+            this.panelRedHistogram.Location = new System.Drawing.Point(352, 391);
+            this.panelRedHistogram.Name = "panelRedHistogram";
+            this.panelRedHistogram.Size = new System.Drawing.Size(256, 256);
+            this.panelRedHistogram.TabIndex = 5;
+            // 
+            // panelGreenHistogram
+            // 
+            this.panelGreenHistogram.Location = new System.Drawing.Point(614, 391);
+            this.panelGreenHistogram.Name = "panelGreenHistogram";
+            this.panelGreenHistogram.Size = new System.Drawing.Size(256, 256);
+            this.panelGreenHistogram.TabIndex = 6;
+            // 
+            // panelBlueHistogram
+            // 
+            this.panelBlueHistogram.Location = new System.Drawing.Point(876, 391);
+            this.panelBlueHistogram.Name = "panelBlueHistogram";
+            this.panelBlueHistogram.Size = new System.Drawing.Size(256, 256);
+            this.panelBlueHistogram.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1156, 678);
+            this.Controls.Add(this.panelBlueHistogram);
+            this.Controls.Add(this.panelGreenHistogram);
+            this.Controls.Add(this.panelRedHistogram);
+            this.Controls.Add(this.pbBlueChannel);
+            this.Controls.Add(this.pbGreenChannel);
+            this.Controls.Add(this.pbRedChannel);
+            this.Controls.Add(this.pbOriginal);
+            this.Controls.Add(this.btnLoadImage);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pbOriginal)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbRedChannel)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbGreenChannel)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbBlueChannel)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnLoadImage;
+        private System.Windows.Forms.PictureBox pbOriginal;
+        private System.Windows.Forms.PictureBox pbRedChannel;
+        private System.Windows.Forms.PictureBox pbGreenChannel;
+        private System.Windows.Forms.PictureBox pbBlueChannel;
+        private System.Windows.Forms.Panel panelRedHistogram;
+        private System.Windows.Forms.Panel panelGreenHistogram;
+        private System.Windows.Forms.Panel panelBlueHistogram;
     }
 }
 
--- Lab2/SecondTask/Form1.cs
+++ Lab2/SecondTask/Form1.cs
@@ -1,20 +1,111 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace SecondTask
 {
     public partial class Form1 : Form
     {
+        private Bitmap redHistogramBitmap;
+        private Bitmap greenHistogramBitmap;
+        private Bitmap blueHistogramBitmap;
+
         public Form1()
         {
             InitializeComponent();
+            btnLoadImage.Click += btnLoadImage_Click;
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnLoadImage_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.Filter = "Image Files|*.jpg;*.jpeg;*.png;*.bmp";
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    Bitmap bitmap = new Bitmap(openFileDialog.FileName);
+                    pbOriginal.Image = bitmap;
+                    pbRedChannel.Image = ExtractChannel(bitmap, 'R');
+                    pbGreenChannel.Image = ExtractChannel(bitmap, 'G');
+                    pbBlueChannel.Image = ExtractChannel(bitmap, 'B');
+
+                    // Создание гистограмм
+                    redHistogramBitmap = CreateHistogramImage(BuildHistogram(bitmap, 'R'), panelRedHistogram.Width, panelRedHistogram.Height);
+                    greenHistogramBitmap = CreateHistogramImage(BuildHistogram(bitmap, 'G'), panelGreenHistogram.Width, panelGreenHistogram.Height);
+                    blueHistogramBitmap = CreateHistogramImage(BuildHistogram(bitmap, 'B'), panelBlueHistogram.Width, panelBlueHistogram.Height);
+
+                    // Отображение гистограмм
+                    panelRedHistogram.BackgroundImage = redHistogramBitmap;
+                    panelGreenHistogram.BackgroundImage = greenHistogramBitmap;
+                    panelBlueHistogram.BackgroundImage = blueHistogramBitmap;
+                }
+            }
+        }
+
+        private Bitmap ExtractChannel(Bitmap bitmap, char channel)
+        {
+            Bitmap result = new Bitmap(bitmap.Width, bitmap.Height);
+            for (int y = 0; y < bitmap.Height; y++)
+            {
+                for (int x = 0; x < bitmap.Width; x++)
+                {
+                    Color pixelColor = bitmap.GetPixel(x, y);
+                    Color newColor = channel switch
+                    {
+                        'R' => Color.FromArgb(pixelColor.R, 0, 0),
+                        'G' => Color.FromArgb(0, pixelColor.G, 0),
+                        'B' => Color.FromArgb(0, 0, pixelColor.B),
+                        _ => Color.Black
+                    };
+                    result.SetPixel(x, y, newColor);
+                }
+            }
+            return result;
+        }
+
+        private int[] BuildHistogram(Bitmap bitmap, char channel)
+        {
+            int[] histogram = new int[256];
+
+            for (int y = 0; y < bitmap.Height; y++)
+            {
+                for (int x = 0; x < bitmap.Width; x++)
+                {
+                    Color pixelColor = bitmap.GetPixel(x, y);
+                    int value = channel switch
+                    {
+                        'R' => pixelColor.R,
+                        'G' => pixelColor.G,
+                        'B' => pixelColor.B,
+                        _ => 0
+                    };
+                    histogram[value]++;
+                }
+            }
+            return histogram;
+        }
+
+        private Bitmap CreateHistogramImage(int[] histogram, int width, int height)
+        {
+            Bitmap histogramBitmap = new Bitmap(width, height);
+            using (Graphics g = Graphics.FromImage(histogramBitmap))
+            {
+                g.Clear(Color.White);
+
+                // Нормализация высоты
+                int max = histogram.Max();
+                for (int x = 0; x < 256; x++)
+                {
+                    int normalizedHeight = (int)((histogram[x] / (float)max) * height);
+                    g.DrawLine(Pens.Black, x, height, x, height - normalizedHeight);
+                }
+            }
+            return histogramBitmap;
         }
     }
 }
