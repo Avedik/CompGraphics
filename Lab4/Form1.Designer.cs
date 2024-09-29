823b52a88e6583d883cbbc76d0b9704173a8f520
Avedik <aviedik.mltykhian@mail.ru>
2024-09-29 08:58:43 +0300

    The first four functions of the program are implemented

--- Lab4/Form1.Designer.cs
+++ Lab4/Form1.Designer.cs
@@ -28,13 +28,179 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.dxLabel = new System.Windows.Forms.Label();
+            this.dyLabel = new System.Windows.Forms.Label();
+            this.chainButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.Control;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(661, 487);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(682, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(125, 40);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Очистить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Смещение"});
+            this.comboBox1.Location = new System.Drawing.Point(769, 108);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(176, 21);
+            this.comboBox1.TabIndex = 6;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Enabled = false;
+            this.textBox1.Location = new System.Drawing.Point(739, 188);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(51, 20);
+            this.textBox1.TabIndex = 8;
+            this.textBox1.Text = "0";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox2.Enabled = false;
+            this.textBox2.Location = new System.Drawing.Point(851, 188);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(51, 20);
+            this.textBox2.TabIndex = 9;
+            this.textBox2.Text = "0";
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.Enabled = false;
+            this.label2.Location = new System.Drawing.Point(679, 165);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(111, 13);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Укажите смещение:";
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button2.Enabled = false;
+            this.button2.Location = new System.Drawing.Point(739, 451);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(163, 48);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "Применить";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label4
+            // 
+            this.label4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(679, 83);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(150, 13);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "Аффинное преобразование:";
+            // 
+            // dxLabel
+            // 
+            this.dxLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.dxLabel.AutoSize = true;
+            this.dxLabel.Enabled = false;
+            this.dxLabel.Location = new System.Drawing.Point(712, 191);
+            this.dxLabel.Name = "dxLabel";
+            this.dxLabel.Size = new System.Drawing.Size(21, 13);
+            this.dxLabel.TabIndex = 15;
+            this.dxLabel.Text = "dx:";
+            // 
+            // dyLabel
+            // 
+            this.dyLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.dyLabel.AutoSize = true;
+            this.dyLabel.Enabled = false;
+            this.dyLabel.Location = new System.Drawing.Point(824, 191);
+            this.dyLabel.Name = "dyLabel";
+            this.dyLabel.Size = new System.Drawing.Size(21, 13);
+            this.dyLabel.TabIndex = 16;
+            this.dyLabel.Text = "dy:";
+            // 
+            // chainButton
+            // 
+            this.chainButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.chainButton.Enabled = false;
+            this.chainButton.Location = new System.Drawing.Point(813, 12);
+            this.chainButton.Name = "chainButton";
+            this.chainButton.Size = new System.Drawing.Size(132, 40);
+            this.chainButton.TabIndex = 17;
+            this.chainButton.Text = "Замкнуть полигон";
+            this.chainButton.UseVisualStyleBackColor = true;
+            this.chainButton.Click += new System.EventHandler(this.chainButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.ClientSize = new System.Drawing.Size(957, 511);
+            this.Controls.Add(this.chainButton);
+            this.Controls.Add(this.dyLabel);
+            this.Controls.Add(this.dxLabel);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Аффинные преобразования";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label dxLabel;
+        private System.Windows.Forms.Label dyLabel;
+        private System.Windows.Forms.Button chainButton;
     }
 }
 
--- Lab4/Form1.cs
+++ Lab4/Form1.cs
@@ -12,9 +12,152 @@ namespace Lab4
 {
     public partial class Form1 : Form
     {
+        enum Transormations { Shift };
+        Bitmap bmp;
+        Graphics g;
+        double[,] transformationMatrix; // матрица преобразования
+        List<Point> list = new List<Point>(); //список точек для полигона
+
         public Form1()
         {
             InitializeComponent();
+            bmp = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            pictureBox1.Image = bmp;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            g = Graphics.FromImage(pictureBox1.Image);
+            g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.HighQuality;
+        }
+
+        // Добавляет очередной элемент к полигону при нажатии мышкой
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            list.Add(new Point(e.X, e.Y));
+            if (list.Count() == 3)
+                chainButton.Enabled = true;
+
+            if (list.Count() == 1)
+            {
+                button2.Enabled = true;
+                ((Bitmap)pictureBox1.Image).SetPixel(e.X, e.Y, Color.Red);
+            }
+            else
+            {
+                var pen = new Pen(Color.Red, 1);
+                g.DrawLine(pen, list[list.Count() - 2], list.Last());
+                pen.Dispose();
+            }
+            pictureBox1.Image = pictureBox1.Image;
+        }
+
+        private void RefreshElements()
+        {
+            // Происходит активация элементов, если в комбобоксе выбрано "Смещение"
+            label2.Enabled = textBox1.Enabled =
+                textBox2.Enabled = dxLabel.Enabled =
+                dyLabel.Enabled = comboBox1.SelectedIndex == (int)Transormations.Shift;
+        }
+
+        // Очистка сцены (удаление всех полигонов)
+        private void Clear()
+        {
+            g.Clear(pictureBox1.BackColor);
+            list.Clear();
+
+            comboBox1.SelectedIndex = 0;
+            RefreshElements();
+            button2.Enabled = chainButton.Enabled = false;
+            pictureBox1.Image = pictureBox1.Image;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Clear();
+        }
+
+        private void ChooseTransformation()
+        {
+            // Выбор аффинного преобразования
+            switch (comboBox1.SelectedIndex)
+            {
+                case (int)Transormations.Shift:
+                    double dX = System.Convert.ToDouble(textBox1.Text);
+                    double dY = -System.Convert.ToDouble(textBox2.Text);
+                    transformationMatrix = new double[,] { { 1.0, 0, 0 }, { 0, 1.0, 0 }, { dX, dY, 1.0 } };
+                    break;
+
+                default:
+                    break;
+            }
+        }
+
+        // Для очищения экрана (холста)
+        private void ClearScreen()
+        {
+            g.Clear(pictureBox1.BackColor);
+            pictureBox1.Image = pictureBox1.Image;
+        }
+
+        // Перемножение матриц
+        private double[,] MatrixMultiplication(double[,] m1, double[,] m2)
+        {
+            double[,] res = new double[m1.GetLength(0), m2.GetLength(1)];
+
+            for (int i = 0; i < m1.GetLength(0); ++i)
+                for (int j = 0; j < m2.GetLength(1); ++j)
+                    for (int k = 0; k < m2.GetLength(0); k++)
+                    {
+                        res[i, j] += m1[i, k] * m2[k, j];
+                    }
+
+            return res;
+        }
+
+        // Применяет преобразование с использованием матриц
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ChooseTransformation();
+
+            List<Point> newList = new List<Point>();
+            foreach (Point p in list)
+            {
+                double[,] point = new double[,] { { p.X * 1.0, p.Y * 1.0, 1.0 } };
+                double[,] res = MatrixMultiplication(point, transformationMatrix);
+                newList.Add(new Point(Convert.ToInt32(Math.Round(res[0, 0])), Convert.ToInt32(Math.Round(res[0, 1]))));
+            }
+            ClearScreen();
+
+            Point prevPoint = newList.First();
+            foreach (Point point in newList)
+            {
+                if (point != prevPoint)
+                {
+                    Pen pen = new Pen(Color.Red, 1);
+                    g.DrawLine(pen, prevPoint, point);
+
+                    prevPoint = point;
+                    pen.Dispose();
+                }
+            }
+
+            list = newList;
+            pictureBox1.Image = bmp;
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            RefreshElements();
+        }
+
+        // Замыкает полигон
+        private void chainButton_Click(object sender, EventArgs e)
+        {
+            g.DrawLine(new Pen(Color.Red, 1), list.Last(), list.First());
+            list.Add(list.First());
+            pictureBox1.Image = pictureBox1.Image;
+            chainButton.Enabled = false;
         }
     }
 }
