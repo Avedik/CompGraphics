94a42b198040057a3c5d28f62c4c80146e1e4e00
Avedik <aviedik.mltykhian@mail.ru>
2024-11-24 12:49:47 +0300

    Create Lab11

---
+++ Lab11/Lab11/main.cpp
@@ -0,0 +1,24 @@
+#include <SFML/Graphics.hpp>
+
+int main()
+{
+    // Создаём окно
+    sf::RenderWindow window(sf::VideoMode(200, 200), "Window");
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        // Цикл обработки событий
+        while (window.pollEvent(event))
+        {
+            // Событие закрытия окна, 
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        // Перерисовка окна
+        window.display();
+    }
+
+    return 0;
+}
