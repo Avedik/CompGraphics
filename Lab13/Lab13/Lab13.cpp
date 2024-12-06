ba78d6222976215f37255a79f64bb97163f154d2
Avedik <aviedik.mltykhian@mail.ru>
2024-12-06 07:28:44 +0300

    Create Lab13

---
+++ Lab13/Lab13/Lab13.cpp
@@ -0,0 +1,25 @@
+﻿#include <SFML/Graphics.hpp>
+
+int main()
+{
+    // Создаём окно
+    sf::RenderWindow window(sf::VideoMode(200, 200), "Window");
+
+    // Главный цикл
+    while (window.isOpen())
+    {
+        sf::Event event;
+        // Цикл обработки событий
+        while (window.pollEvent(event))
+        {
+            // Событие закрытия окна, 
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        // Перерисовка окна
+        window.display();
+    }
+
+    return 0;
+}
